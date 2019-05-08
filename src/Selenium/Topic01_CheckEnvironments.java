32b1f9a03716c15e1b4706276fb6578b657a49fc
Phan Thi Phuc <phucphan753@gmail.com>
2019-05-08 22:12:46 +0700

    Init repository

---
+++ src/Selenium/Topic01_CheckEnvironments.java
@@ -0,0 +1,51 @@
+package Selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic01_CheckEnvironments {
+	WebDriver driver;
+
+  @BeforeClass
+  public void beforeClass() {
+	 String url = "https://www.guru99.com/";
+	 
+    driver = new FirefoxDriver();
+    driver.get(url);
+    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+  }
+  
+  @Test
+  public void TC_01_checkUrl() {
+	  String homePageUrl = driver.getCurrentUrl();
+	  String strToPrint = "Home page url is: " + homePageUrl;
+	  System.out.println(strToPrint);
+	
+	  Assert.assertEquals(homePageUrl, "https://www.guru99.com/");
+	  
+  }
+  
+  @Test
+  public void TC_02_checkTitle() {
+	  String homePageTitle = driver.getTitle();
+	  String expectedUrl = "Meet Guru99 - Free Training Tutorials & Video for IT Courses";
+	  Assert.assertEquals(homePageTitle, expectedUrl);
+	  System.out.println("title is " + homePageTitle);
+	  
+  }
+  
+
+
+  @AfterClass
+  public void afterClass() {
+	  driver.close();
+  }
+
+}
